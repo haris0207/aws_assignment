93e0d4bc926774c01be9aded5f412bfcd2acde42
haris <hariskhan0207@gmail.com>
2023-06-21 01:24:07 +0530

    refactored the code

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ const port=8000;
 
 
 
-const { getSignedUrl } = require("@aws-sdk/cloudfront-signer"); // CJS
+const { getSignedUrl } = require("@aws-sdk/cloudfront-signer");
 
 
-// const s3ObjectKey = "private-content/private.jpeg";
-// const url = `d5uga16mdp5g1.cloudfront.net/car3.jpeg`;
-// const dateLessThan = Math.floor(Date.now() / 1000) + 300 
+
 const privateKey = process.env.PRIVATE_KEY;
 const keyPairId = process.env.KEY_PAIR_ID;
 
@@ -19,14 +17,6 @@ const keyPairId = process.env.KEY_PAIR_ID;
 
 app.get('/',  async(req, res) => {
     try {
-    
-      // const signedUrl = await getSignedUrl({
-      //   url: "https://d5uga16mdp5g1.cloudfront.net/car3.jpeg",
-      //   dateLessThan:new Date(Date.now()+1000*60*5),
-      //   keyPairId,
-      //   privateKey,
-      // });
-      const expires = Math.floor(Date.now() / 1000) + 300; // 5 minutes
       const signedUrl = getSignedUrl({
         url: `http://do2lrnf2jhdxm.cloudfront.net/car2.jpg`,
         dateLessThan:new Date(Date.now()+1000*60*5),
